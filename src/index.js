68ae3e170b20b88bc9be39154125d08a83cbabdd
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2019-08-04 11:56:21 -0300

    Development

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print'
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
-
-    console.log(Data);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
