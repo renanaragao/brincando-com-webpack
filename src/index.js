bb9e4b5e77aef0fa4bdb1c1cc7c54ae7c3a330b6
Renan Aragão <renanferreiradearagao@yahoo.com.br>
2019-08-06 00:31:32 -0300

    lazy load

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
- import { cube } from './math.js';
-
- if (process.env.NODE_ENV !== 'production') {
-     console.log('Looks like we are in development mode!');
- }
+ import _ from 'lodash';
 
  function component() {
-     const element = document.createElement('pre');
-     element.innerHTML = [
-         'Hello webpack!',
-         '5 cubed is equal to ' + cube(5)
-     ].join('\n\n');
+     const element = document.createElement('div');
+     const button = document.createElement('button');
+     const br = document.createElement('br');
+     button.innerHTML = 'Click me and look at the console!';
+     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+     element.appendChild(br);
+     element.appendChild(button);
 
+     // Note that because a network request is involved, some indication
+     // of loading would need to be shown in a production-level site/app.
+     button.onclick = e =>
+         import ( /* webpackChunkName: "print" */ './print').then(module => {
+             const print = module.default;
+
+             print();
+         });
      return element;
  }
-
  document.body.appendChild(component());
